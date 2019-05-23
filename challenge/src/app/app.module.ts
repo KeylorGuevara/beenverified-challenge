2b7b31ebb8b44208dce2a4c6a34e1c7113506445
kguevaraj <keylorguevara@hotmail.es>
2019-05-23 07:03:47 -0600

    Creating a Login and Register.
    The component for login and registrar was maked.

--- challenge/src/app/app.module.ts
+++ challenge/src/app/app.module.ts
@@ -3,14 +3,35 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponentComponent } from './login-component/login-component.component';
+import { AdminComponentComponent } from './admin-component/admin-component.component';
+import { HomeComponentComponent } from './home-component/home-component.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponentComponent,
+    AdminComponentComponent,
+    HomeComponentComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot([
+      {
+        path: 'login',
+        component: LoginComponentComponent
+      },
+      {
+        path: 'admin',
+        component: AdminComponentComponent
+      },
+      {
+        path: '',
+        component: HomeComponentComponent
+      }
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
